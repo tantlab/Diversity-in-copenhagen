71fd26cfdfe1e99e2fcf76a345b39d9c211594b2
Edoardo Guido <edoardo.guido.ext@accurat.it>
2019-12-11 14:37:20 +0100

    Updates

---
+++ precache-manifest.6c8dc774f01cb69aa2bfa9c5243feeb1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03d7268c6ee02ddb8d05faa21d92cb2",
+    "url": "/diversity-in-copenhagen/index.html"
+  },
+  {
+    "revision": "4441ab15a3cf9b7def50",
+    "url": "/diversity-in-copenhagen/static/css/2.e3daf8d4.chunk.css"
+  },
+  {
+    "revision": "591a3dfbd0ea4e2ac03a",
+    "url": "/diversity-in-copenhagen/static/css/main.e2f1db6e.chunk.css"
+  },
+  {
+    "revision": "4441ab15a3cf9b7def50",
+    "url": "/diversity-in-copenhagen/static/js/2.03d14036.chunk.js"
+  },
+  {
+    "revision": "591a3dfbd0ea4e2ac03a",
+    "url": "/diversity-in-copenhagen/static/js/main.d93d8d8a.chunk.js"
+  },
+  {
+    "revision": "35162fc2c105e1494385",
+    "url": "/diversity-in-copenhagen/static/js/runtime-main.c7754fa0.js"
+  },
+  {
+    "revision": "0dceb602c4e794086152cf92003d80d7",
+    "url": "/diversity-in-copenhagen/static/media/din_light.0dceb602.otf"
+  },
+  {
+    "revision": "67b1386626199182c59f28b280de8196",
+    "url": "/diversity-in-copenhagen/static/media/din_regular.67b13866.otf"
+  },
+  {
+    "revision": "0e225597a59caad314e793a4727ca1ed",
+    "url": "/diversity-in-copenhagen/static/media/din_regular_italic.0e225597.otf"
+  }
+]);
